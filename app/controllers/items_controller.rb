8531f1e82bb219468fbd1eb7b7f803ef6ce559aa
AkikoShinohara <69193404+AkikoShinohara@users.noreply.github.com>
2020-08-28 19:04:49 +0900

    ログイン画面へリダイレクトするため

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,14 +2,13 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index]
   def index
   end
-    
-
+  
   private
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
     else
-      
+
     end
   end
 end
