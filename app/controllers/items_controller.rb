ec3e2a87796a957778b80ddeb08b7ba1b04e6038
AkikoShinohara <69193404+AkikoShinohara@users.noreply.github.com>
2020-09-02 00:26:23 +0900

    ログインしていないユーザーはログイン先へ遷移させる変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,9 +18,10 @@ class ItemsController < ApplicationController
   end
 
   private
+
   def move_to_index
     unless user_signed_in?
-      redirect_to action: :index
+      redirect_to new_user_session_path
     else
     end
   end
