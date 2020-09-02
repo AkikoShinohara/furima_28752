4fdea58ea4e06eabc3ec372b16bfcd33a091f391
AkikoShinohara <69193404+AkikoShinohara@users.noreply.github.com>
2020-09-02 11:49:05 +0900

    userコントローラー作成

--- app/javascript/items.js
+++ app/javascript/items.js
@@ -2,7 +2,7 @@ function price(){
 const priceInput = document.getElementById("item-price");
 const add_tax = document.getElementById("add-tax-price");
 const profit = document.getElementById("profit");
-    priceInput.addEventListener("keyup", () => {
+    priceInput.addEventListener('keyup', () => {
       const value = priceInput.value;
 
       if (value >= 300 && value <= 9999999){
@@ -11,11 +11,11 @@ const profit = document.getElementById("profit");
           add_tax.textContent = fee;
           profit.textContent = gains;
     } else {
-        let fee = '-';
-        let gains = '-';
+        let fee = '0';
+        let gains = '0';
         add_tax.textContent = fee;
         profit.textContent = gains;
     }
   });
 }
-window.addEventListener( "load", price);
+window.addEventListener('load', price);
