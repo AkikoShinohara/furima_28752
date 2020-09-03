07fa306a1c2fbecf2db7cee8e4f4b895cbe8a13c
AkikoShinohara <69193404+AkikoShinohara@users.noreply.github.com>
2020-09-03 12:17:10 +0900

    edit, update定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  before_action :set_item, only: [:show]
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.includes(:user).order("created_at ASC")
   end
@@ -19,6 +19,21 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+  end
+  
+  def update
+    if @item.user_id == current_user.id
+      if @item.update(item_params)
+        redirect_to item_path(@item.id)
+      else
+        render :edit
+      end
+    else
+      redirect_to action: :edit
+    end
   end
 
   private
