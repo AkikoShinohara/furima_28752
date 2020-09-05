1d2ad21f0b3c358f44099113d4e1a97b415d0169
AkikoShinohara <69193404+AkikoShinohara@users.noreply.github.com>
2020-09-05 13:50:50 +0900

    Formオブジェクト

--- README.md
+++ README.md
@@ -78,15 +78,16 @@ Things you may want to cover:
 
 ## addresses テーブル
 
-| Column         | Type      | Options                        |
-| -------------- | --------- | ------------------------------ |
-| postal_code    | string    | null: false                    |
-| prefectures_id | integer   | null: false                    |
-| cities         | string    | null: false                    |
-| address        | string    | null: false                    |
-| building       | string    |                                |
-| phone_number   | string    | null: false                    |
-| user           | reference | null: false, foreign_key: true |
+| Column          | Type      | Options                        |
+| --------------- | --------- | ------------------------------ |
+| postal_code     | string    | null: false                    |
+| prefecture      | integer   | null: false                    |
+| city            | string    | null: false                    |
+| address         | string    | null: false                    |
+| house_number    | string    | null: false                    |
+| building_number | string    |                                |
+| phone_number    | string    | null: false                    |
+| payment         | reference | null: false, foreign_key: true |
 
 ### Association
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_04_033119) do
+ActiveRecord::Schema.define(version: 2020_09_05_034704) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,19 @@ ActiveRecord::Schema.define(version: 2020_09_04_033119) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "postal_code", default: "", null: false
+    t.integer "prefecture", null: false
+    t.string "city", default: "", null: false
+    t.string "house_number", default: "", null: false
+    t.string "building_name", default: ""
+    t.string "phone_number", default: "", null: false
+    t.bigint "payment_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["payment_id"], name: "index_addresses_on_payment_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -76,6 +89,7 @@ ActiveRecord::Schema.define(version: 2020_09_04_033119) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "addresses", "payments"
   add_foreign_key "items", "users"
   add_foreign_key "payments", "items"
   add_foreign_key "payments", "users"
