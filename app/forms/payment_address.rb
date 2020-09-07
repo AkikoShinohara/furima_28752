3624730aa2719e3e1435d03b83b61ed2cd568a90
AkikoShinohara <69193404+AkikoShinohara@users.noreply.github.com>
2020-09-07 22:43:22 +0900

    バリデーション変更

--- app/forms/payment_address.rb
+++ app/forms/payment_address.rb
@@ -4,11 +4,12 @@ class PaymentAddress
   attr_accessor :postal_code, :prefecture, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :city, :house_number, :phone_number
+    validates :postal_code, :city, :house_number,:phone_number
     validates :token
   end
+  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :prefecture, numericality: { other_than: 0, message: "can't be blank" }
+  validates :phone_number,  format: { with: /\A\d{11}\z/ }
 
   def save
     payment = Payment.create(user_id: user_id, item_id: item_id)
